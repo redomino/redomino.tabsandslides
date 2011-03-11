86cfb15516d02ac1118ae860e31e63b1721ef178
maurizio.lupo <maurizio.lupo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-11 10:54:21 +0000

    added configuration of js as a browser resource

---
+++ redomino/tabsandslides/browser/redomino.tabsandslides.config.js
@@ -0,0 +1,48 @@
+var tabsandslides = {};
+
+//
+// GALLERY
+//
+tabsandslides.gallery = {};
+tabsandslides.gallery.config_boxscrollable = {horizontal:true,
+//                                            number_items:4, 
+                                              minheight:200, 
+                                              minwidth:210,
+                                              easing:'easeInOutCirc'}
+
+//
+// IMAGE TABS
+//
+tabsandslides.image_tabs = {};
+tabsandslides.image_tabs.config = {};
+tabsandslides.image_tabs.config_boxscrollable = {horizontal:true,
+//                                               number_items:4, 
+                                                 minheight:200, 
+                                                 minwidth:220,
+                                                 easing:'easeInOutCirc'};
+
+//
+// TABS
+//
+tabsandslides.tabs = {};
+
+tabsandslides.tabs.config = {};
+
+//
+// SLIDESHOW
+//
+
+tabsandslides.slideshow = {};
+
+tabsandslides.slideshow.config = {
+    // enable "cross-fading" effect
+    effect: 'fade',
+    fadeOutSpeed: "slow",
+    rotate: true
+};
+
+tabsandslides.slideshow.slideshow_config = {clickable:false};
+
+
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '0.6'
 
 setup(name='redomino.tabsandslides',
       version=version,
