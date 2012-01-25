066320efd8741eadf7666b40b996f86a98e9f612
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2012-01-25 14:53:21 +0100

    Moved README back. ops..

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.6.5'
 setup(name='redomino.tabsandslides',
       version=version,
       description="jquerytools tabs and slide implementations",
-      long_description=open("README.rst").read() + "\n" +
+      long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
